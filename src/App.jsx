edcfaa713aa5a4fbaa61b6aa24609d79f3d4f9d3
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-23 17:26:17 -0800

    setting up view file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from './views/Layout/Layout';
+import Home from './views/Home/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,5 @@
+import './Home.css';
+
+export default function Home() {
+  return <div className="Home">Home</div>;
+}
