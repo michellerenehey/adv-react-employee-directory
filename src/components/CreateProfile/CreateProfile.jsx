0d87137054e632480379895f20df75bf0c040295
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-23 18:52:20 -0800

    notes for profiles and setting up home page

---
+++ src/components/CreateProfile/CreateProfile.jsx
@@ -0,0 +1,16 @@
+// connect to css
+
+// if no profile exists
+// 'you must create a profile'
+
+// name: string
+// email: string (disabled; not editable, but still visible in the form)
+// birthday: date (you can use <input type="date">)
+// bio: text (you can use <textarea>)
+
+// if profile exists
+// profile page
+// edit button
+// edit button populates 'create profile'
+
+// MAKE THESE COMPONENTS ONE INSTEAD OF TWO?
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,12 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <h1>Welcome to the Acme Inc. Employee Directory!</h1>
+      <Link to="/register">Create Account</Link>
+      <Link to="/login">Sign In</Link>
+    </div>
+  );
 }
